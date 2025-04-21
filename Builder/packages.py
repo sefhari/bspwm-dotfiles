83a874d414b00dff513fe04e2cb56e2abb9eb39f
QrV1t9 <qrvite@gmail.com>
2025-04-22 00:40:04 +0300

    feat: added some packages

--- Builder/packages.py
+++ Builder/packages.py
@@ -2,18 +2,17 @@
 
 BASE_PACKAGES = [
     "tumbler", "ffmpegthumbnailer", "lsd", "alacritty", "bat", "brightnessctl", "calc",
-    "automake", "blueman", "bluez", "bluez-utils", "dunst", "fakeroot", "feh", "firefox",
+    "automake", "dunst", "fakeroot", "feh", "firefox",
     "dpkg", "gcc", "gedit", "git", "gnu-netcat", "htop", "btop", "lxappearance",
     "mat2", "mpd", "mpv", "thunar", "ncmpcpp", "neofetch", "network-manager-applet", "nitrogen",
-    "pamixer", "papirus-icon-theme", "pavucontrol", "polybar", "autoconf", "mpc", "pulseaudio",
-    "pulseaudio-alsa", "python-pyalsa", "ranger", "redshift", "reflector", "rofi", "rofi-calc", "calcurse",
+    "pamixer", "papirus-icon-theme", "pavucontrol", "polybar", "autoconf", "mpc",
+    "ranger", "redshift", "reflector", "rofi", "rofi-calc", "calcurse",
     "rofi-emoji", "scrot", "sudo", "slop", "tree", "unrar", "zip", "unzip", "uthash", "xarchiver",
     "xfce4-power-manager", "xfce4-settings", "xorg-xbacklight", "zathura", "zathura-djvu", "zathura-pdf-mupdf",
     "cmake", "clang", "gzip", "imagemagick", "make", "openssh", "shellcheck",
     "usbutils", "picom", "alsa-plugins", "alsa-tools", "alsa-utils", "ffmpeg",
     "p7zip", "gparted", "sshfs", "xclip", "gpick", "wget", "ueberzug", "netctl", "light", "libreoffice",
-    "breeze", "ttf-jetbrains-mono", "ttf-jetbrains-mono-nerd", "ttf-fira-code",
-    "ttf-iosevka-nerd", "thunderbird", "copyq", "micro"
+    "breeze", "thunderbird", "copyq", "micro"
 ]
 
 DEV_PACKAGES = [
@@ -25,7 +24,7 @@ DEV_PACKAGES = [
 ]
 
 AUR_PACKAGES = [
-    "cava", "i3lock-color", "64gram-desktop-bin", "birdtray", "nekoray-bin"
+    "cava", "i3lock-color", "64gram-desktop-bin", "birdtray", "nekoray-bin", "all-repository-font"
 ]
 
 GNOME_OFFICIAL_TOOLS = [
